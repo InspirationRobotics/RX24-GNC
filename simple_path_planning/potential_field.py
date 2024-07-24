8a695afbb62bf7300fe733f39865075cec309878
Dat-Bois <eeshvij@gmail.com>
2024-07-24 12:32:12 -0700

    Added path gen

--- simple_path_planning/potential_field.py
+++ simple_path_planning/potential_field.py
@@ -6,7 +6,9 @@ from typing import List
 
 class Goal:
     def __init__(self, position : np.array, gain : float, radius : float = 10):
-        self.position = position
+        if isinstance(position, list):
+            position = np.array(position)
+        self.position : np.ndarray = position
         self.radius = radius
         self.gain = gain
 
@@ -32,7 +34,6 @@ def repulsive_force(current_position : np.array, obstacle : Obstacle) -> np.arra
     if distance < obstacle.radius:
         influence = sigmoid(1-(distance / obstacle.radius)) ** 2
         force = obstacle.gain * influence * (1.0 / (distance)) * (current_position - obstacle.position)
-        print(influence)
     magnitude = np.linalg.norm(force)
     direction = np.arctan2(force[1], force[0])
     return force, (magnitude, direction)
@@ -55,6 +56,9 @@ def potential_field(current_position : np.array, goals : List[Goal], obstacles :
     direction = np.arctan2(force[1], force[0])
     if magnitude > 1.0:
         force = 2 * (force / magnitude)
+
+    # add a small random component to the force
+    force += 0.1 * np.random.randn(2)
     return force, (magnitude, direction)
 
 # Define the obstacle map
@@ -62,25 +66,50 @@ map_size = (720, 1280)
 MAP = np.zeros((map_size[0], map_size[1], 3), np.uint8)
 
 # Define the start and end goal positions
-# start = np.array([0, 0], dtype=np.float32)
-# end_goal = Goal(np.array([450, 450], dtype=np.float32), 0.1)
 start = np.array([0, 360], dtype=np.float32)
-end_goal = Goal(np.array([1000, 360], dtype=np.float32), 0.02)
+end_goal = Goal(np.array([1000, 360], dtype=np.float32), 0.1)
+
+# goal points:
+def temp_goal_points(start_pos, end_pos, amount, *, variation=15):
+    if isinstance(start_pos, np.ndarray):
+        start_pos = start_pos.copy()
+    if isinstance(end_pos, np.ndarray):
+        end_pos = end_pos.copy()
+    start_pos[0]+=40
+    end_pos[0]-=40
+    spacing = (end_pos[0] - start_pos[0]) / amount
+    goal_points = []
+    for i in range(amount):
+        goal_points.append(Goal([start_pos[0], start_pos[1]], 0.1))
+        start_pos[0]+=spacing
+        start_pos[1]+=np.random.randint(-variation,variation)
+    return goal_points
+
+# obstacle points:
+def temp_obstacle_points(goal_points : List[Goal]):
+    obstacle_points = []
+    for point in goal_points:
+        pos1 = point.position.copy()
+        pos2 = point.position.copy()
+        pos1[1]-=20
+        pos2[1]+=20
+        obstacle_points.append(Obstacle(pos1, 5))
+        obstacle_points.append(Obstacle(pos2, 5))
+    return obstacle_points
 
 # Define the goals
 goals: List[Goal] = [
-    # Goal(np.array([200, 75], dtype=np.float32), 0.1),
-    # Goal(np.array([150, 125], dtype=np.float32), 0.1),
     end_goal
 ]
 
+goal_points = temp_goal_points(start, end_goal.position, 20, variation=40)
+goals.extend(goal_points)
+
 # Define the obstacles
 obstacles: List[Obstacle] = [
-    # Obstacle(np.array([100, 75]), 20),
-    # Obstacle(np.array([200, 200]), 30),
-    # Obstacle(np.array([300, 300]), 40)
-    Obstacle(np.array([300, 370]), 40),
+    # Obstacle([300, 350], 20)
 ]
+obstacles.extend(temp_obstacle_points(goal_points))
 
 while True:
     # Get the force and information
@@ -92,6 +121,10 @@ while True:
     # Draw the map
     map = MAP.copy()
     cv2.circle(map, (int(start[0]), int(start[1])), 5, (0, 0, 255), -1)
+    # draw an arrow showing the vector with direction and magnitude
+    cv2.arrowedLine(map, (int(start[0]), int(start[1])), (int(start[0]+force[0]*info[0]), int(start[1]+force[1]*info[0])), (0, 0, 255), 2)
+
+
     for goal in goals:
         cv2.circle(map, (int(goal.position[0]), int(goal.position[1])), 5, (0, 255, 0), -1)
     for obstacle in obstacles:
@@ -105,10 +138,13 @@ while True:
     if key == ord('q'):
         break
 
+    if key == ord(' '):
+        cv2.waitKey(0)
+
     # Check if the current position is the goal
     for i in range(len(goals)-1, -1, -1):
         if np.linalg.norm(start - goals[i].position) < goals[i].radius:
-            print('Goal reached!')
+            # print('Goal reached!')
             goals.pop(i)
             break
 
