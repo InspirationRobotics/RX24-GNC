e93e3833df9526638a37c73a2c143af8c5d34e70
Dat-Bois <eeshvij@gmail.com>
2024-09-17 14:04:36 -0700

    Built new test_control

--- control/dev/test_control.py
+++ control/dev/test_control.py
@@ -1,6 +1,3 @@
-# Just a simple test to check if the motor_core module is working properly
-# We test by sending a command to the client to move the motor
-
 '''
 Taken from motor_core.py. Details the available commands for the motor_core module.
 
@@ -14,6 +11,7 @@ Global Interface:
     - If not in waypoint or poshold mode, this is the target vector.
  - target_position: A list with 2 values, the lat and lon components of the target position.
     - If in waypoint mode, this is the target position. This is persistent until used.
+ - mission_name: A string with the mission name. If a valid name is given, the waypoints will be executed sequentially
  - target_heading: A single value, the target heading of the rover.
     - If in poshold or manual mode, this is the target heading.
  - set_mode: A single value, the target mode of the rover.
@@ -26,10 +24,23 @@ Global Interface:
 
 '''
 
+# This is similar to test_control.py, but this has an inbuilt heartbeat, so the gi never times out in motor_core
+# It also has simpler functionality allowing for the following features:
+# - Set mode
+# - Set target vector
+# - Set target position
+# - Set target heading
+# - Send mission name (for waypoint mode)
+
+# The heartbeat is sent every 1.5 seconds and is an unused li variable: heartbeat
+# When additional data needs to be sent, it is just added on to the heartbeat
+# Only one command can be added to the heartbeat at a time
 
 import time
 
+from threading import Thread, Lock
 from typing import Dict, Any, List, Tuple
+
 from comms_core import Server, Logger
 from comms_core import CustomSocketMessage as csm
 
@@ -37,196 +48,148 @@ MANUAL = 0
 POSHOLD = 1
 WAYPOINT = 2
 
-def set_mode(mode, msg : Dict[str, Any]):
-   msg['set_mode'] = mode
-   return msg
-
-def set_target_vector(vector : Tuple[float, float], msg : Dict[str, Any]):
-   msg['target_vector'] = vector
-   return msg
-
-def set_target_position(position : Tuple[float, float], msg : Dict[str, Any]):
-   msg['target_position'] = position
-   return msg
-
-def set_target_heading(heading : float, msg : Dict[str, Any]):
-   msg['target_heading'] = heading
-   return msg
-
-def emergency_stop(stop : bool, msg : Dict[str, Any]):
-   msg['emergency_stop'] = stop
-   return msg
-
-def set_debug(debug : bool, msg : Dict[str, Any]):
-   msg['debug'] = debug
-   return msg
-
-def set_raw_motor_power(power : Tuple[float, float], msg : Dict[str, Any]):
-   msg['raw_motor_power'] = power
-   return msg
+class TestControl(Logger):
     
-def dummy_callback(data, addr):
-   pass
-   # print(f"Got data from {addr}")
+   def __init__(self):
+      super().__init__(__name__)
+      self.server = Server()
+      self.server.start()
 
-# Create a server object
-server = Server(default_callback=dummy_callback)
-server.start()
+      self.msg_lock = Lock()
+      self.msg = {}
 
-# Create a logger object
-logger = Logger("test_control")
+      self.active = True
+      self.heartbeat_thread = Thread(target=self.send_hearbeat_thread, daemon=True)
 
+      self.heartbeat_thread.start()
 
+   def stop(self):
+      self.active = False
+      self.heartbeat_thread.join(3)
+      self.server.stop()
 
-def process_input(data, msg):
-   if data == "exit" or data == "quit":
-      return False
+   def __del__(self):
+      self.stop()
 
-# Create an interactive terminal to send commands to the server
+   def set_mode(self, mode : str):
+      try:
+         mode = int(mode)
+      except ValueError:
+         self.error(f"Invalid mode {mode}. Mode should be 0, 1, or 2")
+         return
+      if mode not in [0, 1, 2]:
+         self.error(f"Invalid mode {mode}. Mode should be 0, 1, or 2")
+      with self.msg_lock:
+         self.msg = {}
+         self.msg['set_mode'] = mode
 
-# Available commands:
-# - 1. set_mode -> 0, 1, 2
-# - 2. set_target_vector -> [x, y]
-# - 3. set_target_position -> [lat, lon]
-# - 4. set_target_heading -> heading
-# - 5. emergency_stop -> True, False
-# - 6. set_debug -> True, False
-# - 7. set_raw_motor_power -> [left_pwr, right_pwr] (-1 to 1)
+   def set_target_vector(self, vector : Tuple[str, str]):
+      try:
+         x = float(vector[0])
+         y = float(vector[1])
+      except ValueError:
+         self.error(f"Invalid vector {vector}.")
+         return
+      with self.msg_lock:
+         self.msg = {}
+         self.msg['target_vector'] = [x,y]
 
-def ask_for_input(msg):
-   data = input(msg)
-   if data == "exit" or data == "quit":
-      return False
-   if data == "back":
-      return None
-   return data
+   def set_target_position(self, position : Tuple[str, str]):
+      try:
+         x = float(position[0])
+         y = float(position[1])
+      except ValueError:
+         self.error(f"Invalid position {position}.")
+      with self.msg_lock:
+         self.msg = {}
+         self.msg['target_position'] = [x, y]
 
-def process_input(data, to_send : dict):
+   def set_target_heading(self, heading : str):
+      try:
+         heading = float(heading)
+      except ValueError:
+         self.error(f"Invalid heading {heading}.")
+      with self.msg_lock:
+         self.msg = {}
+         self.msg['target_heading'] = heading
 
-   if data == "1":
-      mode = ask_for_input("Enter the mode (0, 1, 2): ")
-      if mode == False or mode == None:
-         return mode
-      to_send = set_mode(int(mode), to_send)
-      return to_send
+   def set_mission_name(self, mission_name : str):
+      with self.msg_lock:
+         self.msg = {}
+         self.msg['mission_name'] = mission_name
+
+   def set_emergency_stop(self, stop : str):
+      try:
+         stop = int(stop)
+         if stop not in [0, 1]:
+            raise ValueError
+      except ValueError:
+         self.error(f"Invalid emergency stop {stop}.")
+      stop = bool(stop)
+      with self.msg_lock:
+         self.msg = {}
+         self.msg['emergency_stop'] = stop
+
+   def send_hearbeat_thread(self):
+      while self.active:
+         with self.msg_lock:
+            self.msg['heartbeat'] = 00
+            self.server.send(csm.encode(self.msg))
+            self.msg = {}
+         time.sleep(1.5)
+
+   def process_input(self, data):
+
+      if data == "exit" or data == "quit":
+         return False
+      
+      if data == "1":
+         mode = input("Enter the mode (0, 1, 2): ")
+         self.set_mode(mode)
    
-   if data == "2":
-      x = ask_for_input("Enter the target x-vector: ")
-      if x == False or x == None:
-         return x
-      y = ask_for_input("Enter the target y-vector: ")
-      if y == False or y == None:
-         return y
-      to_send = set_target_vector([float(x), float(y)], to_send)
-      return to_send
-   
-   if data == "3":
-      # lat = ask_for_input("Enter the target latitude: ")
-      # if lat == False or lat == None:
-      #    return lat
-      # lon = ask_for_input("Enter the target longitude: ")
-      # if lon == False or lon == None:
-      #    return lon
-      #32.91426471087947, -117.10186806192723
+      if data == "2":
+         x = input("Enter the target x-vector: ")
+         y = input("Enter the target y-vector: ")
+         self.set_target_vector([x, y])
 
-      # lat = 32.91426471087947
-      # lon = -117.10186806192723
+      if data == "3":
+         lat = input("Enter the target latitude: ")
+         lon = input("Enter the target longitude: ")
+         self.set_target_position([lat, lon])
 
-      point1 = (32.91514313674598, -117.10053347345651)
-      point2 = (32.91519019795828, -117.10160586313923)
-      point3 = (32.91474180227439, -117.10084539786419)
-      point4 = (32.91469620258607, -117.10029768180299)  
-      points = [point1, point2, point3, point4]
-      cnt = -1
+      if data == "4":
+         heading = input("Enter the target heading: ")
+         self.set_target_heading(heading)
+
+      if data == "5":
+         mission_name = input("Enter the mission name: ")
+         self.set_mission_name(mission_name)
+
+      if data == "6":
+         stop = input("Enter the emergency stop (0, 1): ")
+         self.set_emergency_stop(stop)
+
+      return True
+
+   def handle_input(self):
+      print("Welcome to the motor control test terminal!")
+      print("Available commands:")
+      print("1. set_mode -> 0, 1, 2")
+      print("2. set_target_vector -> [x, y]")
+      print("3. set_target_position -> [lat, lon]")
+      print("4. set_target_heading -> heading")
+      print("5. Mission Name -> mission_name")
+      print("6. emergency_stop -> True, False")
+      print("exit or quit-> Exit the terminal")
+      print("Enter the command: ")
+
       while True:
-         val = input()
-         if val == " ":
-            cnt+=1
-         if val == "q":
+         data = input(">>> ")
+         if not self.process_input(data):
             break
-         if cnt > 3:
-            break
-         lat, lon = points[cnt]
-         to_send = set_target_position([float(lat), float(lon)], to_send)
-         server.send(csm.encode(to_send))
-      return {}
-   
-   if data == "4":
-      heading = ask_for_input("Enter the target heading: ")
-      if heading == False or heading == None:
-         return heading
-      to_send = set_target_heading(float(heading), to_send)
-      return to_send
-   
-   if data == "5":
-      stop = ask_for_input("Enter True or False: (1, 0)")
-      if stop == False or stop == None:
-         return stop
-      stop = True if stop == "1" else False
-      to_send = emergency_stop(stop, to_send)
-      return to_send
-   
-   if data == "6":
-      debug = ask_for_input("Enter True or False: (1, 0)")
-      if debug == False or debug == None:
-         return debug
-      debug = True if debug == "1" else False
-      to_send = set_debug(debug, to_send)
-      return to_send
-   
-   if data == "7":
-      left = ask_for_input("Enter the left motor power: (-1 to 1)")
-      if left == False or left == None:
-         return left
-      right = ask_for_input("Enter the right motor power: (-1 to 1)")
-      if right == False or right == None:
-         return right
-      to_send = set_raw_motor_power([float(left), float(right)], to_send)
-      return to_send
-   
-   if data == "exit" or data == "quit":
-      return False
+      print("Exiting...")
 
-   if data == "send":
-      server.send(csm.encode(to_send))
-      if "target_position" in to_send.keys() or "target_heading" in to_send.keys():
-         while input() != "q":
-            server.send(csm.encode(to_send))
-      return {}
-    
-   return False
-
-while True:
-   print("Welcome to the motor control test terminal!")
-   print("Available commands:")
-   print("1. set_mode -> 0, 1, 2")
-   print("2. set_target_vector -> [x, y]")
-   print("3. set_target_position -> [lat, lon]")
-   print("4. set_target_heading -> heading")
-   print("5. emergency_stop -> True, False")
-   print("6. set_debug -> True, False")
-   print("7. set_raw_motor_power -> [left_pwr, right_pwr] (-1 to 1)")
-   print("send -> Send the command to the server")
-   print("exit or quit-> Exit the terminal")
-   print("back -> Go back to this screen")
-   print("Enter the command: ")
-   to_send = {}
-   while True:
-      data = ask_for_input(">> ")
-      if data == False:
-         print("Exiting the terminal...")
-         server.stop()
-         exit()
-      to_send = process_input(data, to_send)
-      if to_send == False:
-         print("Exiting the terminal...")
-         server.stop()
-         exit()
-      if to_send == None:
-         break
-
-
-'''
-32.9513
--117.10118
-'''
+if __name__ == "__main__":
+   control = TestControl()
+   control.handle_input()
+   control.stop()
