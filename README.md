50edbfc4f1306c57730cc12c3635f7ef80079cc0
Dat-Bois <eeshvij@gmail.com>
2024-10-01 22:41:13 -0700

    Restructure

---
+++ README.md
@@ -0,0 +1,4 @@
+To install packages:
+
+Navigate into `mission` directory and run:
+```pip3 install -e .```
---
+++ mission/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mission_core',
+    version='0.1',
+    description='Core mission package for Inspiration RobotX',
+    author='Eesh Vij, Carson Rae',
+    packages=find_packages(include=['mission_core'])
+)
