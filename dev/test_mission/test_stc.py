2d6f066d0520a0b55e261fedc6c0a58026814bf3
Dat-Bois <eeshvij@gmail.com>
2024-10-20 12:54:00 -0700

    Scan the code test ready

---
+++ dev/test_mission/test_stc.py
@@ -0,0 +1,19 @@
+import cv2
+import time
+import numpy as np
+from mission_core import MissionHandler, STCMission
+
+if __name__ == "__main__":
+
+    mission = STCMission()
+    mission_handler = MissionHandler(mission)
+    mission_handler.start_mission()
+    while True:
+        try:
+            time.sleep(0.1)
+        except KeyboardInterrupt:
+            break
+    
+    cv2.destroyAllWindows()
+    mission_handler.stop()
+    
--- mission/mission_core/__init__.py
+++ mission/mission_core/__init__.py
@@ -1,3 +1,6 @@
 from .mission import MissionHandler
 from .mission_node import MissionNode, PositionData, OccupancyData
 from .missions.mission_template import SimpleMission
+
+
+from .missions.STC_mission import STCMission
